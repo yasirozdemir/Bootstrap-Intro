2768d99f96c90c447bc850fd88f60ff327cd9a4a
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-01-10 23:06:05 +0300

    style updates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function countNumberOfAlbums() {
   let cards1 = document.querySelectorAll("#favorites_container .card");
   let reviewAlbum = document.querySelectorAll;
   ("#review_container img");
-  let cards2 = document.querySelectorAll("#worst-albums_container .card");
+  let cards2 = document.querySelectorAll("#hall-of-shame_container .card");
   let theBestAlbum = document.querySelectorAll("#the-best_container img");
   let tapestryAlbums = document.querySelectorAll("#tapestry_container img");
 
