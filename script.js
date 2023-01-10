0b1453642879bfa4afe8158082685d4e7e2b5de9
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-01-10 18:52:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function countNumberOfAlbums() {
+  let cards1 = document.querySelectorAll("#favorites_container .card");
+  let reviewAlbum = document.querySelectorAll;
+  ("#review_container img");
+  let cards2 = document.querySelectorAll("#worst-albums_container .card");
+  let theBestAlbum = document.querySelectorAll("#the-best_container img");
+  let tapestryAlbums = document.querySelectorAll("#tapestry_container img");
+
+  return (totalNumOfAlbums =
+    cards1.length +
+    cards2.length +
+    reviewAlbum.length +
+    theBestAlbum.length +
+    tapestryAlbums.length);
+}
+
+function showTotalNumberOfAlbums() {
+  document.getElementById(
+    "number-of-albums"
+  ).innerText = `Number of albums: ${countNumberOfAlbums()}`;
+}
+
+window.onload = () => {
+  showTotalNumberOfAlbums();
+};
